9f7a8953328e1f42c9d68135b1f2f62cb6ead8b0
Nolan Phillips <ncphillips.19@gmail.com>
2019-11-25 17:26:00 -0400

    create new posts

--- src/PageList/index.tsx
+++ src/PageList/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { usePageCollection } from "../Firebase";
-import { useLocalForm, usePlugin } from "react-tinacms";
+import React, { useState } from "react";
+import { usePageCollection, useFirestore } from "../Firebase";
+import { useLocalForm, usePlugin, usePlugins } from "react-tinacms";
+import { AddContentPlugin } from "tinacms";
 
 export function PageList() {
+  usePageCreatorPlugin();
+
   const pages = usePageCollection();
 
   return (
@@ -20,18 +23,36 @@ const PageListItem = (props: any) => {
   return <li>{page.title}</li>;
 };
 
+const PAGE_FIELDS = [
+  { name: "title", label: "Title", component: "text" },
+  { name: "published", label: "Publisehd", component: "toggle" },
+  { name: "body", label: "Body", component: "markdown" }
+];
+
 function usePageForm(page: any) {
   return useLocalForm({
     id: page.id,
     initialValues: page,
     label: page.title,
-    fields: [
-      { name: "title", label: "Title", component: "text" },
-      { name: "published", label: "Publisehd", component: "toggle" },
-      { name: "body", label: "Body", component: "markdown" }
-    ],
+    fields: PAGE_FIELDS,
     onSubmit() {
       alert("TODO");
     }
   });
+}
+
+function usePageCreatorPlugin() {
+  const firestore = useFirestore();
+
+  const [plugin] = useState<AddContentPlugin<any>>(() => ({
+    __type: "content-button",
+    name: "Page",
+    fields: PAGE_FIELDS,
+    async onSubmit(formValues) {
+      if (firestore) {
+        firestore.collection("pages").add(formValues);
+      }
+    }
+  }));
+  usePlugins(plugin);
 }
